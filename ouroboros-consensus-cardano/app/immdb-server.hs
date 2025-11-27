27a934dde6fc6258cc9ae7dd97146bd6208ab00a
Vince Reuter <vince.reuter@tweag.io>
2025-11-27 11:42:47 +0100

    first-pass at host address flexibility through CLI for immdb-server

--- ouroboros-consensus-cardano/app/immdb-server.hs
+++ ouroboros-consensus-cardano/app/immdb-server.hs
@@ -7,29 +7,47 @@ import Cardano.Crypto.Init (cryptoInit)
 import qualified Cardano.Tools.DBAnalyser.Block.Cardano as Cardano
 import Cardano.Tools.DBAnalyser.HasAnalysis (mkProtocolInfo)
 import qualified Cardano.Tools.ImmDBServer.Diffusion as ImmDBServer
+import Data.Bifunctor (first)
 import Data.Void
+import Data.Word (Word8)
 import Main.Utf8 (withStdTerminalHandles)
 import qualified Network.Socket as Socket
 import Options.Applicative
 import Ouroboros.Consensus.Node.ProtocolInfo (ProtocolInfo (..))
+import Text.Read (readMaybe)
 
 main :: IO ()
 main = withStdTerminalHandles $ do
   cryptoInit
-  Opts{immDBDir, port, configFile} <- execParser optsParser
+  Opts{immDBDir, addr, port, configFile} <- execParser optsParser
   let sockAddr = Socket.SockAddrInet port hostAddr
        where
         -- could also be passed in
-        hostAddr = Socket.tupleToHostAddress (127, 0, 0, 1)
+        hostAddr = Socket.tupleToHostAddress addr
       args = Cardano.CardanoBlockArgs configFile Nothing
   ProtocolInfo{pInfoConfig} <- mkProtocolInfo args
   absurd <$> ImmDBServer.run immDBDir sockAddr pInfoConfig
 
+type HostAddr = (Word8, Word8, Word8, Word8)
+
 data Opts = Opts
   { immDBDir :: FilePath
+  , addr :: HostAddr
   , port :: Socket.PortNumber
   , configFile :: FilePath
   }
+
+parseAddr :: String -> Either String HostAddr
+parseAddr s = first contextualize $ traverse tryParse chunks >>= extractResult
+ where
+  contextualize msg = "Cannot parse address (" ++ s ++ "): " ++ msg
+  chunks = reverse $ map reverse $ go s [] []
+  go [] curr acc = curr : acc
+  go ('.' : t) curr acc = go t [] (curr : acc)
+  go (h : t) curr acc = go t (h : curr) acc
+  tryParse sub = maybe (Left ("cannot parse component '" ++ sub ++ "'")) Right (readMaybe sub)
+  extractResult [sub1, sub2, sub3, sub4] = Right (sub1, sub2, sub3, sub4)
+  extractResult subs = Left (show (length subs) ++ " (not 4) components")
 
 optsParser :: ParserInfo Opts
 optsParser =
@@ -44,6 +62,14 @@ optsParser =
           [ long "db"
           , help "Path to the ImmutableDB"
           , metavar "PATH"
+          ]
+    addr <-
+      option (eitherReader parseAddr) $
+        mconcat
+          [ long "addr"
+          , help "Address to serve at"
+          , value (127, 0, 0, 1)
+          , showDefault
           ]
     port <-
       option auto $
@@ -60,4 +86,4 @@ optsParser =
           , help "Path to config file, in the same format as for the node or db-analyser"
           , metavar "PATH"
           ]
-    pure Opts{immDBDir, port, configFile}
+    pure Opts{immDBDir, addr, port, configFile}
