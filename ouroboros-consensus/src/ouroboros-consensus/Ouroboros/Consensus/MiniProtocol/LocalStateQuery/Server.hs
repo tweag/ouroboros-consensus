b77f8274916383944666bf00c3fc911201795073
Alex Mason <alex.mason@tweag.io>
2026-02-11 11:57:55 +1100

    Support explicit unleashing

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/LocalStateQuery/Server.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/LocalStateQuery/Server.hs
@@ -36,72 +36,43 @@ localStateQueryServer cfg leashingPointVar getCurrentChainAnchorPoint getView =
   where
     idle :: ServerStIdle blk (Point blk) (Query blk) m ()
     idle = ServerStIdle {
-          recvMsgAcquire = \tgt leashed -> do
+          recvMsgAcquire = \tgt leashId -> do
             traceM $ "idle: handle acquire"
-            handleAcquire tgt leashed
-        , recvMsgDone    = return ()
+            handleAcquire tgt leashId
+        , recvMsgDone    = do
+            traceM "idle: done"
+            return ()
         }
 
     handleAcquire :: Target (Point blk)
                   -> Maybe LeashID
                   -> m (ServerStAcquiring blk (Point blk) (Query blk) m ())
-    handleAcquire mpt leashed = do
-      traceM $ "handleAcquire: start " <> show leashed
+    handleAcquire mpt mLeashId = do
+      traceM $ "handleAcquire: start " <> show mLeashId
       getView mpt >>= \case
-        -- case if we want to leash and there is a leashing point var
-        Right forker | Just leashId <- leashed ->
-            atomically $ putLeashPoint forker mpt leashId
+        Right forker | Just leashId <- mLeashId -> do
+            atomically (acquireLeash mpt leashId) >>= \case
+              Just err -> pure $ SendMsgFailure err idle
+              Nothing  -> pure $ SendMsgAcquired $ acquired (Just leashId) forker
         Right forker -> pure $ SendMsgAcquired $ acquired Nothing forker
         Left PointTooOld{} -> pure $ SendMsgFailure AcquireFailurePointTooOld idle
         Left PointNotOnChain -> pure $ SendMsgFailure AcquireFailurePointNotOnChain idle
 
-    requestedPoint mpt = case mpt of
-        SpecificPoint p -> pure p
-        _ -> getCurrentChainAnchorPoint
-
-    putLeashPoint forker mpt messageLeashId = do
-        traceM $ "SETTING LEASHING POINT VARIABLE"
-        readTVar leashingPointVar >>= \case
-          Nothing -> do
-            -- TODO: What happens if the point is too old?
-            leashState <- (\p -> (messageLeashId,p)) <$> requestedPoint mpt
-            writeTVar leashingPointVar $ Just leashState
-            traceM $ "START LEASHING, LEASHING POINT VARIABLE IS " <> show leashState
-            pure $ SendMsgAcquired $ acquired (Just messageLeashId) forker
-
-          Just current@(currentLeashId, currentLeashPoint)
-            | messageLeashId /= currentLeashId -> do
-              traceM $ "ATTEMPTED TO LEASH WITH WRONG ID: " <> show (currentLeashId, messageLeashId)
-              pure $ SendMsgFailure AcquireFailurePointStateIsBusy idle
-            | otherwise -> do
-                requestedLeashPoint <- requestedPoint mpt
-                let newLeashState = (messageLeashId, requestedLeashPoint)
-                if requestedLeashPoint < currentLeashPoint
-                then do
-                  traceM $ "ATTEMPTED TO LEASH BACKWARDS: "
-                        <> show current
-                        <> " => "
-                        <> show newLeashState
-                  pure $ SendMsgFailure AcquireFailurePointStateIsBusy idle
-                else do
-                  writeTVar leashingPointVar $ Just newLeashState
-                  traceM $ "UPDATED LEASHING, LEASHING POINT VARIABLE IS " <> show newLeashState
-                  pure $ SendMsgAcquired $ acquired (Just messageLeashId) forker
-
     acquired :: Maybe LeashID
              -> ReadOnlyForker' m blk
              -> ServerStAcquired blk (Point blk) (Query blk) m ()
-    acquired leashed forker = ServerStAcquired {
-          recvMsgQuery     = handleQuery leashed forker
+    acquired clientLeashId forker = ServerStAcquired {
+          recvMsgQuery     = do
+            traceM "acquire: query"
+            handleQuery clientLeashId forker
         , recvMsgReAcquire = \mp -> do
-            traceM $ "acquired: re acquire, leash " <> show leashed
+            traceM $ "acquired: re acquire, leash " <> show clientLeashId
             close
-            handleAcquire mp leashed
-        , recvMsgRelease   = do
-            traceM $ "acquired: release, leash " <> show leashed
+            handleAcquire mp clientLeashId
+        , recvMsgRelease   = \msgLeashId -> do
+            traceM $ "acquired: release, leash " <> show msgLeashId
             close
-            -- TODO: Is this right? I think we need to make unleashing explicit
-            atomically $ writeTVar leashingPointVar Nothing
+            releaseLeash msgLeashId
             return idle
         }
       where
@@ -112,6 +83,51 @@ localStateQueryServer cfg leashingPointVar getCurrentChainAnchorPoint getView =
       -> ReadOnlyForker' m blk
       -> Query blk result
       -> m (ServerStQuerying blk (Point blk) (Query blk) m () result)
-    handleQuery leashed forker query = do
+    handleQuery leashId forker query = do
       result <- Query.answerQuery cfg forker query
-      return $ SendMsgResult result (acquired leashed forker)
+      return $ SendMsgResult result (acquired leashId forker)
+
+    -- TODO: This should always compare with getCurrentChainAnchorPoint to
+    -- check if the point is too old.
+    requestedPoint mpt = case mpt of
+        SpecificPoint p -> pure p
+        _ -> getCurrentChainAnchorPoint
+
+    acquireLeash mpt messageLeashId = do
+        traceM $ "SETTING LEASHING POINT VARIABLE"
+        readTVar leashingPointVar >>= \case
+          Nothing -> do
+            -- TODO: What happens if the point is too old?
+            leashPoint <- requestedPoint mpt
+            let newLeashState = (messageLeashId, leashPoint)
+            traceM $ "START LEASHING, LEASHING POINT VARIABLE IS " <> show newLeashState
+            writeTVar leashingPointVar $ Just newLeashState
+            pure Nothing
+
+          Just currentState@(currentLeashId, currentLeashPoint)
+            | messageLeashId /= currentLeashId -> do
+              traceM $ unwords ["ATTEMPTED TO UPDATE LEASH WITH WRONG ID, current:",
+                                show currentLeashId, "provided:", show messageLeashId]
+              pure $ Just AcquireFailurePointStateIsBusy
+
+            | otherwise -> do
+                requestedLeashPoint <- requestedPoint mpt
+                let newState = (messageLeashId, requestedLeashPoint)
+                if requestedLeashPoint < currentLeashPoint
+                then do
+                  traceM $ unwords ["ATTEMPTED TO LEASH BACKWARDS:", show currentState, "=>", show newState]
+                  pure $ Just AcquireFailurePointStateIsBusy
+                else do
+                  traceM $ "UPDATED LEASHING, LEASHING POINT VARIABLE IS " <> show newState
+                  writeTVar leashingPointVar $ Just newState
+                  pure Nothing
+
+    releaseLeash :: Maybe LeashID -> m ()
+    releaseLeash Nothing = pure ()
+    releaseLeash (Just leashId) = atomically $
+      readTVar leashingPointVar >>= \case
+        Just x@(currentId,_)
+          | leashId == currentId -> do
+            traceM $ "LEASH RELEASED: " <> show x
+            writeTVar leashingPointVar Nothing
+        _ -> pure ()
