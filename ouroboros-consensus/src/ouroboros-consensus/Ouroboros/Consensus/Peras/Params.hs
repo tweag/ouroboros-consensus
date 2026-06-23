7dd350c37c290f53926fd7d300fb0f8539e1fc6d
Adithya Kumar <adithya.kumar@tweag.io>
2026-06-23 16:02:20 +0530

    Update default peras params

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Peras/Params.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Peras/Params.hs
@@ -160,26 +160,26 @@ defaultPerasParams =
   PerasParams
     { -- ceil(T_heal + T_cq) / perasRoundLength) as per the design document
       perasIgnoranceRounds =
-        PerasIgnoranceRounds 487
+        PerasIgnoranceRounds 10
     , -- ceil(T_heal + T_cq + T_cp) / perasRoundLength) + 1 as per the design document
       perasCooldownRounds =
-        PerasCooldownRounds 1928
+        PerasCooldownRounds 25
     , -- must be between 30 and 900 as per the design document
       perasBlockMinSlots =
-        PerasBlockMinSlots 90
+        PerasBlockMinSlots 1
     , -- equal to perasIgnoranceRounds as per the design document
       perasCertMaxRounds =
-        PerasCertMaxRounds 487
+        PerasCertMaxRounds 10
     , perasCertArrivalThreshold =
-        PerasCertArrivalThreshold 30
+        PerasCertArrivalThreshold 10
     , perasRoundLength =
-        PerasRoundLength 90
+        PerasRoundLength 60
     , perasWeight =
-        PerasWeight 15
+        PerasWeight 100
     , perasQuorumWeightThreshold =
         PerasQuorumWeightThreshold (3 / 4)
     , perasQuorumWeightThresholdSafetyMargin =
-        PerasQuorumWeightThresholdSafetyMargin (2 / 100)
+        PerasQuorumWeightThresholdSafetyMargin 0
     , perasTargetCommitteeSize =
         Committee.TargetCommitteeSize 800
     }
