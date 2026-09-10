875d9b460bfc6d73289152894bdbd6cfbba2c6e7
Erin van der Veen <erin.vanderveen@tweag.io>
2026-09-10 10:55:49 +0200

    disable waiting for cert diffusion in ThreadNet

--- ouroboros-consensus-diffusion/src/unstable-diffusion-testlib/Test/ThreadNet/Network.hs
+++ ouroboros-consensus-diffusion/src/unstable-diffusion-testlib/Test/ThreadNet/Network.hs
@@ -135,7 +135,7 @@ import Ouroboros.Network.PeerSelection.Governor
   ( makePublicPeerSelectionStateVar
   )
 import Ouroboros.Network.PeerSelection.PeerMetric (nullMetric)
-import Ouroboros.Network.PerasSupport (PerasSupport (PerasSupported, PerasUnsupported))
+import Ouroboros.Network.PerasSupport (PerasSupport (PerasUnsupported))
 import Ouroboros.Network.Point (WithOrigin (..))
 import qualified Ouroboros.Network.Protocol.ChainSync.Type as CS
 import Ouroboros.Network.Protocol.KeepAlive.Type
@@ -1427,7 +1427,8 @@ directedEdgeInner
                       "ChainSync"
                       (\_s _ -> RestartChainSyncTerminated)
                       (\_s () -> RestartChainSyncTerminated)
-                      ( \a v e c -> NTN.aChainSyncClient a (if v >= NodeToNodeV_16 then PerasSupported else PerasUnsupported) v e c
+                      -- TODO: ThreadNet currently doesn't start up certificate object diffusion, so we pass PerasUnsupported to avoid waiting indefinitely.
+                      ( \a v e c -> NTN.aChainSyncClient a PerasUnsupported v e c
                       )
                       NTN.aChainSyncServer
                       chainSyncMiddle
